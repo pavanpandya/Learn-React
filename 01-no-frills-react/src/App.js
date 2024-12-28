24071126d6a98d6c80b8b766c0f9f00fd6119243
Pavan Pandya <pavanpandya1311@gmail.com>
2024-12-27 19:35:33 -0500

    Module-1

--- 01-no-frills-react/src/App.js
+++ 01-no-frills-react/src/App.js
@@ -1,3 +1,31 @@
+// // Writing my first react app experience 😊
+// const App = () => {
+//   return React.createElement(
+//     "div",
+//     {}, // or you can put Null as well.
+//     React.createElement("h1", {}, "Hey, this is my first react app!")
+//   );
+// };
+
+// const container = document.getElementById("root");
+// const root = ReactDOM.createRoot(container);
+// root.render(React.createElement(App));
+
+// // React is all about making components.
+// // We are staying, Inside root, stamp the "App" component once (instance of component) and we can do that
+// // as many times we want to.
+
+// // -----------------------------------------------------------------------
+
+// // Version 1.0
+// const Pizza = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "The Veg Pizza"),
+//     React.createElement("p", {}, "Onion, Bell Pepper, Tomatos, Cheese"),
+//   ]);
+// };
+
+// // Version 2.0
 const Pizza = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
@@ -7,18 +35,15 @@ const Pizza = (props) => {
 
 const App = () => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement("h1", {}, "MyPizzaShop"),
+    // React.createElement(Pizza, {}),
     React.createElement(Pizza, {
-      name: "The Pepperoni Pizza",
-      description: "Mozzarella Cheese, Pepperoni",
+      name: "The Veg Pizza",
+      description: "Onion, Bell Pepper, Tomatos, Cheese",
     }),
     React.createElement(Pizza, {
-      name: "The Hawaiian Pizza",
-      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
-    }),
-    React.createElement(Pizza, {
-      name: "The Big Meat Pizza",
-      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+      name: "The Seven Cheesy Pizza",
+      description: "Seven different types of cheese.",
     }),
   ]);
 };
